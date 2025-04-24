6064bb9cf017abfd5f52e20de805e86dbad7d276
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-04-24 21:25:42 +0530

    Create twoSum.java

---
+++ src/twoPointer_SlidingWindow/twoSum.java
@@ -0,0 +1,20 @@
+package twoPointer_SlidingWindow;
+
+public class twoSum {
+    public static void main (String[] args){
+        int [] arr = {3, 7, 9, 10, 15, 30};
+        int k = 17;
+        System.out.println (isPresent(arr, k));
+    }
+// if given that the array is sorted, we can use the Binary Search as well.
+    public static boolean isPresent(int[] arr, int k){
+        for (int i = 0; i < arr.length; i++){
+            for (int j = arr.length - 1; j > 0; j--){
+                if (arr[i] + arr[j] == k){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

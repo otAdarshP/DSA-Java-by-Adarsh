fe1be0afa9412a0f2e1329ed7f2ecb6bad439e4e
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-05-18 16:50:17 +0530

    Update Main.java

--- src/SelectionSort/Main.java
+++ src/SelectionSort/Main.java
@@ -1,6 +1,37 @@
 package SelectionSort;
 
+import java.util.Arrays;
+
+import static java.util.Collections.swap;
+
 public class Main {
+    public static void main (String[] args){
+        int[] arr = {3, 4, -3, 1, 3};
+        selectMaxElement(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    static void selectMaxElement (int [] arr){
+        for (int i = 0; i < arr.length; i++){
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last + 1);
+            swap (arr, maxIndex, last);
+        }
+    }
 
+    static void swap (int[] arr, int first, int second){
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+    }
+
+    static int getMaxIndex (int[] arr, int start, int end) {
+        int maxIndex = start;
+        for (int i = start; i < end; i++){
+            if (arr[i] > arr[maxIndex]){
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
 }

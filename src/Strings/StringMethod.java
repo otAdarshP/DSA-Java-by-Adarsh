2d792901076082ac2357f0f4dca502b9b8166ee6
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-06-05 01:15:24 +0530

    Strings

--- src/Strings/StringMethod.java
+++ src/Strings/StringMethod.java
@@ -6,5 +6,8 @@ public class StringMethod {
     public static void main (String[] args){
         String name = "Adarsh";
         System.out.println (Arrays.toString(name.toCharArray())); // to print the characters in a string
+        System.out.println(Arrays.toString(name.toUpperCase().toCharArray())); // characters in capslock
+        System.out.println(name.indexOf('a')); // finds the index of the character
+
     }
 }
---
+++ src/Strings/StringPalindrome.java
@@ -0,0 +1,7 @@
+package Strings;
+
+public class StringPalindrome {
+    public static void main (String[] args){
+
+    }
+}

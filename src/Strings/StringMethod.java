bd9a1c93a1cd13d5f630275c8cea69304596715c
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-06-05 01:06:46 +0530

    Create StringMethod.java

---
+++ src/Strings/StringMethod.java
@@ -0,0 +1,10 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class StringMethod {
+    public static void main (String[] args){
+        String name = "Adarsh";
+        System.out.println (Arrays.toString(name.toCharArray())); // to print the characters in a string
+    }
+}

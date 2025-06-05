33b2b858898971a58e6bc3768b8324596981b7a4
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-06-05 23:14:49 +0530

    updates

--- src/Pattern/Patttern3.java
+++ src/Pattern/Patttern3.java
@@ -2,13 +2,13 @@ package Pattern;
 
 public class Patttern3 {
     public static void main (String[] args){
-        design3(4);
+        design3(7);
     }
 
 
     public static void design3 (int pattern3){
-        for (int col = 0; col <= pattern3; col++){
-            for (int row = 0; row <= col; row++){
+        for (int row = 0; row <= pattern3; row++){
+            for (int col = 0; col <= pattern3-row; col++){
                 System.out.print("* ");
             }
             System.out.println();

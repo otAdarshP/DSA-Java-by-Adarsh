75a2ed2903703a41c2d5ecba18ce20b36c47ea54
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-07-16 03:31:19 +0530

    updates

---
+++ src/Recursion/SumOfArrayDigits.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class SumOfArrayDigits {
+    public static void main (String[] args){
+        int [] nums = {1,2,3,4};
+        int index = 0;
+        int answer = sumOfDigits(nums, 0);
+        System.out.println(answer);
+
+    }
+    static int sumOfDigits(int[] nums, int index){
+        if (index == nums.length){
+            return 0;
+        }
+
+        return nums[index] + sumOfDigits(nums, index + 1);
+    }
+}

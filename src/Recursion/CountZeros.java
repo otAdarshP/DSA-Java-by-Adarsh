57c9679e952ecb9fd1ede6345b8cb49fc9b13621
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-07-18 02:15:34 +0530

    Update CountZeros.java

--- src/Recursion/CountZeros.java
+++ src/Recursion/CountZeros.java
@@ -1,20 +1,27 @@
 package Recursion;
 
 public class CountZeros {
-    public static void main (String[] args){
-        String num = "10000000009000";
-
+    public static void main (String[] args) {
+        String num = "1000009000";
         int [] freq = new int [10];
-        
-    }
 
-    public static String zeroCount (String num){
-        if (num.length() == 0 || num.lines() == 1){
-            return num;
+        zeroCount(num, 0, freq);
+        for (int i = 0; i < 10; i++){
+            System.out.println(i + "=> " + freq[i]);
         }
 
-        // recursion:
+    }
+    public static String zeroCount (String strNum, int index, int [] freq){
+        if (index == strNum.length()){
+            return strNum;
+        }
 
-        int count += 1;
+//        char [] charArray = strNum.toCharArray();
+//        char charStrNum = charArray[index];
+//        or in place of the above just use below, which is more time and memory space efficient
+        char charStrNum = strNum.charAt(index);
+        int digit = charStrNum - '0';
+        freq[digit]++;
+        return zeroCount(strNum, index+1, freq);
     }
 }

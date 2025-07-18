c8c07b81aa4e5fc8a6b6bf05888a8cddf43a8bae
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-07-19 03:14:06 +0530

    RBS

--- src/BinarySearch2D/SearchIn2D.java
+++ src/BinarySearch2D/SearchIn2D.java
@@ -1,9 +1,38 @@
 package BinarySearch2D;
 
 public class SearchIn2D {
-    public static void main (String [] args){
-        int [][] matrix = new int [4][4];
+    public static void main (String [] args) {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        int target = 78;
+        boolean answer = searchMatrix(matrix, target);
+        System.out.println(answer);
     }
 
+    public static boolean searchMatrix (int [][] matrix, int target){
 
+        int m;
+        int n;
+
+        m = matrix.length;
+        n = matrix[0].length;
+
+        int start = 0;
+        int end = m * n - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            int midElement = matrix[mid / n] [mid % n];
+
+            if (midElement == target){
+                return true;
+            }
+            if (midElement > target) {
+                end = mid - 1;
+            }
+            if (midElement < target) {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
 }

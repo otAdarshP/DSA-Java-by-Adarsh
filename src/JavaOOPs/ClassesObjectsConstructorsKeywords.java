9b3173bc3c0efc892220f07d54e12222f1ccadd6
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-07-29 20:27:36 +0530

    backtracking

---
+++ src/JavaOOPs/ClassesObjectsConstructorsKeywords.java
@@ -0,0 +1,21 @@
+package JavaOOPs;
+
+
+// Lecture  of the Object-Oriented Programming in Java
+//              CLASSES -- OBJECTS -- CONSTRUCTORS -- KEYWORDS
+
+
+public class ClassesObjectsConstructorsKeywords {
+    public static void main (String[] args){
+
+    }
+}
+
+// syntax of a class
+class Student {
+    int [] rno = new int [5];
+    int [] phNo = new int [5];
+    String [] name = new String [5];
+
+    Student Adarsh;
+}
---
+++ src/LeetCodeBacktracking/SudokuHashSet.java
@@ -0,0 +1,26 @@
+package LeetCodeBacktracking;
+
+import java.util.HashSet;
+
+class Solution {
+
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char number = board[i][j];
+                if (number != '.') {
+                    if (!seen.add(number + " in row " + i) ||
+                            !seen.add(number + " in col " + j) ||
+                            !seen.add(number + " in box " + i/3 + "-" + j/3)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
+

ab403f227b062d67e031cae4753c9574ca960d4a
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-08-12 14:26:44 +0530

    Update RootFinding.java

--- src/Trees/RootFinding.java
+++ src/Trees/RootFinding.java
@@ -1,4 +1,7 @@
 package Trees;
+import java.util.Queue;
+import java.util.LinkedList;
+
 
 public class RootFinding {
     public static class Node {
@@ -34,16 +37,57 @@ public class RootFinding {
         if (root == null){
             return;
         }
-        System.out.println(root.data);
+        System.out.print(root.data + " ");
         preOrder (root.left);
         preOrder(root.right);
     }
 
+    public static void InOrder (Node root){
+        // steps:
+        // 1. Right subtree; 2. Root; 3. Left subtree
+
+        if (root == null){
+            return;
+        }
+        InOrder (root.left);
+        System.out.println(root.data);
+        InOrder(root.right);
+    }
+
+    public static void PostOrder (Node root){
+        if (root == null){
+            return;
+        }
+        PostOrder (root.left);
+        PostOrder(root.right);
+        System.out.println(root.data);
+    }
+
+    // main --> Level Order traversal
+    public static void LevelOrder (Node root){
+        if (root == null){
+            return;
+        }
+
+        Queue <Node> queue = new LinkedList <>();
+        queue.add (root);
+        queue.add (null);
+
+        while (!queue.isEmpty()){
+            if (queue == null){
+                
+            }
+            else{
+
+            }
+        }
+
+    }
     public static void main (String [] args){
         int [] nodes = {1,2,3,4,-1,-1,5,-1,-1,6,-1,-1,7,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        Node root = tree.preOrder(root);
+        preOrder(root);
         System.out.println(root.data);
     }
 

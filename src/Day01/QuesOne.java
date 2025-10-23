59059a51f28e95dd87b11cd5c22d4f3d002322da
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-10-24 02:12:36 +0530

    Update QuesOne.java

--- src/Day01/QuesOne.java
+++ src/Day01/QuesOne.java
@@ -2,6 +2,12 @@ package Day01;
 
 public class QuesOne {
     public static void main(String[] args) {
+        int[] arr = new int[] {-2,1,-3,4,-1,2,1,-5,4};
 
+
+    }
+
+    public int maxSubArray (int[] nums){
+        
     }
 }

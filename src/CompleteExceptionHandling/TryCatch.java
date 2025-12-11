17e5bbf61bae00fc3c42624ad546a2d95e29cee4
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-12-12 02:28:24 +0530

    Update TryCatch.java

--- src/CompleteExceptionHandling/TryCatch.java
+++ src/CompleteExceptionHandling/TryCatch.java
@@ -1,12 +1,30 @@
 package CompleteExceptionHandling;
 
+import org.w3c.dom.ls.LSOutput;
+
+// THIS IS FOR TRY/CATCH/FINALLY/THROW/THROWS IN THE EXCEPTION HANDLING
 public class TryCatch {
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
+        int[] arr = {1, 2, 3};
 
+        try{
+            int index = arr[5];
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            throw new IndexOutOfBoundsException("You have exceeded the size of the array, please try again.");
+        }
+        finally{
+            System.out.println("We are glad to see you here!");
+        }
+
+        try{
+            int ans = arr[2] / 2;
+            if (ans == 0){
+                System.out.println("Its an even number");
+            }
+        }
+        catch (Exception e){
+            throw new Exception("the output is odd");
+        }
     }
-
-    public static void number (){
-
-    }
-    
 }

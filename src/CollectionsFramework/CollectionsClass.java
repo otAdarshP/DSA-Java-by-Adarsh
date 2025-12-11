b80f373e1e35e6546fbec0b4f4b7a218f685a3c8
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-12-11 18:54:32 +0530

    exception handling

--- src/CollectionsFramework/CollectionsClass.java
+++ src/CollectionsFramework/CollectionsClass.java
@@ -1,4 +1,21 @@
 package CollectionsFramework;
+import java.util.*;
 
 public class CollectionsClass {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        Arrays.sort(arr);
+        int index = Arrays.binarySearch(arr, 4); // works only in sorted array
+       // Collections.max(arr);
+       // Collections.sort(arr); // won't work
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        Collections.sort (list); // works
+
+        // Collections.sort() or .max() or any other,
+        // they only works on the objects and not on primitive data types.
+
+
+
+    }
 }
---
+++ src/CompleteExceptionHandling/TryCatch.java
@@ -0,0 +1,12 @@
+package CompleteExceptionHandling;
+
+public class TryCatch {
+    public static void main (String[] args){
+
+    }
+
+    public static void number (){
+
+    }
+    
+}
